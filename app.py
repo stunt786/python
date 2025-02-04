c8bcb3a944d79ac4bafcf4cc52e0f3e697b8bc66
Prakash Bhandari <rosyprakash786@gmail.com>
2025-02-04 12:00:14 +0545

    latestqq

--- app.py
+++ app.py
@@ -5,8 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def webout():
 	
-	return '<h2>This is wonderful Project</h2>'
-	return '<h3>We are awsome/h3>'
+	return '<h2>This is wonderful Project and nice.</h2>'
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=7000)
